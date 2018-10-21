ab8ff55f07c35967b14450212b72ddd92c6bb09f
Kolachow <kolas3000@gmail.com>
2018-10-21 14:30:38 +0200

    Gotowy do uzycia

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,12 @@ public class Main {
         CustomList<String> customList = new CustomLinkedList<>();       //utworzenie wlasnej listy
 
         customList.add("Jeden");
+        System.out.println();
         customList.add("Dwa");
+        System.out.println();
         customList.add("Trzy");
+        System.out.println();
+        customList.add("Cztery");
 
         List<String> list = new LinkedList<>();     //utworzenie listy
 
